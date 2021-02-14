275489b641890efdf88f633100f74771f1d402b8
Steve Coonrod <StevenCCoonrod@gmail.com>
2021-02-13 22:24:49 -0600

    End of Day 1

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name:'The Dude Sweater',
+            category:'Sweaters',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'El Duderino',
+            rating: 5,
+            numReviews: 10,
+            description: 'Hand-knitted sweater perfect for sippin white Russians while rollin on the Shabbos.'
+        },
+        {
+            _id: '2',
+            name:'LeFemme Mens sudo-sweater',
+            category:'Sweaters',
+            image: '/images/p2.jpg',
+            price: 110,
+            countInStock: 3,
+            brand: 'LeFemme',
+            rating: 4,
+            numReviews: 6,
+            description: 'High quality sweater that will barely keep you warm while bluring the lines of gender based clothing.'
+        },
+        {
+            _id: '3',
+            name:'Plain Blue T',
+            category:'Shirts',
+            image: '/images/p3.jpg',
+            price: 90,
+            countInStock: 0,
+            brand: 'TooMuch Clothing',
+            rating: 3,
+            numReviews: 13,
+            description: 'Extremely high-quality navy t-shirt unencumbered by designs or logos.'
+        },
+        {
+            _id: '4',
+            name:'Plain White T',
+            category:'Shirts',
+            image: '/images/p4.jpg',
+            price: 120,
+            countInStock: 0,
+            brand: 'TooMuch Clothing',
+            rating: 4,
+            numReviews: 10,
+            description: 'Extremely high-quality white t-shirt unencumbered by designs or logos.'
+        },
+        {
+            _id: '5',
+            name:'Dark Gray T',
+            category:'Shirts',
+            image: '/images/p5.jpg',
+            price: 80,
+            countInStock: 39,
+            brand: 'SimplyFancy Inc',
+            rating: 3.5,
+            numReviews: 3,
+            description: 'This simple t-shirt is so cool you\'ll just stand around staring at it'
+        },
+        {
+            _id: '1',
+            name:'Plain White Hoodie',
+            category:'Sweaters',
+            image: '/images/p6.jpg',
+            price: 150,
+            countInStock: 16,
+            brand: 'MakinBank Clothes',
+            rating: 4.5,
+            numReviews: 6,
+            description: 'High-quality hoodie you will think is really awesome until you realize wearing it at all will distroy it.'
+        },
+    ]
+}
+
+export default data;
